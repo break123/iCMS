ac5b7b57bcf94ec9471c4aee42babece40f4d1ce
coolmoo <idreamsoft@qq.com>
2011-12-29 17:49:35 +0800

    首页模块
    Signed-off-by: coolmoo <idreamsoft@qq.com>

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* iCMS - i Content Management System
+* Copyright (c) 2007-2012 idreamsoft.com iiimon Inc. All rights reserved.
+*
+* @author coolmoo <idreamsoft@qq.com>
+* @site http://www.idreamsoft.com
+* @licence http://www.idreamsoft.com/license.php
+* @version 6.0.0 (2012-02-14)
+*/
+require (dirname(__file__) . '/global.php');
+//require_once iCMS_MODULE.'/index/index.class.php';
+//$iCMS->module	= new index;
+//$iCMS->moduleRun();
+
+$iCMS->run();
